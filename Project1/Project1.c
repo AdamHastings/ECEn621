b3a8b35cbe3df5395dcb01529a294c31e10e04d0
Adam Hastings <adamh5@cardamom.ee.byu.edu>
2018-01-23 14:37:53 -0700

    string issues

--- Project1/Project1.c
+++ Project1/Project1.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include <math.h>
+#define M_PI 3.14159265358979323846
 
 
-double computeSine(int N, int j) {
-	return 0;
+double computeSine(double delta) {
+	return delta - (delta*delta*delta)/6 +(delta*delta*delta*delta*delta) / 120;
 }
 
 
 int main() {
-  int N_array[3] = {20, 50, 100};
+  int N_array[1] = {20};
 
   for (int i=0; i < sizeof(N_array)/sizeof(N_array[0]); i++) {
 	int N = N_array[i];
-	int length = snprintf( NULL, 0, "%d", N ) + 4;
-	char* str = malloc( length + 1 );
+	// int length = snprintf( NULL, 0, "%d", N ) + 4;
+	// char* str = malloc( length + 1 );
 	snprintf( str, length + 1, "%d.txt", N );
 
 	FILE *fp;
 	fp = fopen(str, "w+");
 	free(str);
 
+	double delta = M_PI / (2 * N);
+
+	double sine, cosine;
+
 	for (int j=0; j < N*4; j++) {
-		int value_j = computeSine(N, j);
-		fprintf(fp, "%d\n", value_j);
+		if (j == 0) {
+			sine = 0;
+			cosine = 1;
+		} else if (j == N) {
+			sine = 1;
+			cosine = 0;
+		} else if (j == 2*N) {
+			sine = 0;
+			cosine = -1;
+		} else if (j == 3*N) {
+			sine = -1;
+			cosine = 0;
+		} else {
+			sine += computeSine(delta*j);
+		}
+		printf("%0d\n", sine);
+		fprintf(fp, "%d\n", sine);
 	}
 
 	fclose(fp);
