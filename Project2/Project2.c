b8747cbbcf3c06a620ef78ec08f0a6ef586948cb
Adam Hastings <adamh5@fennel.ee.byu.edu>
2018-02-02 13:59:34 -0700

    fixed-point underway

--- Project2/Project2.c
+++ Project2/Project2.c
@@ -1,20 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 #define M_PI 3.14159265358979323846
 #define PI_TIMES_10exp10 31415926535
+// Pi in fixed point: 11.00100100001111110111
+// Pi in 8.24 fixed-point: 00000011 00100100 00111111 01110000
 
 
 int computeDivision(int N, int D) {
      int interations = 10;
-     int N = 1;
-     int D = 1;
      for (int i=0; i<interations; i++) {
           N = N*(2-D);
           D = D*(2-D);
      }
      int Q = N;
      return Q;
+}
+
+int get_MSB (int x) {
+	int i = 0;
+	while (x > 1) {
+		i++;
+		x = x >> 1;
+	}
+	return i;
 }
 
 double computeDelta_sine(double delta) {
@@ -33,52 +43,63 @@ double recurrence_cos(double sin_theta, double cos_theta, double sine, double co
      return cosine*cos_theta - sine*sin_theta;
 }
 
+
+
+
+// Let's pick an arbitrary 8.24 fixed-point numbering system.
+// Also, we can convert to float for printing!
 int main() {
-     int N_array[3] = {20, 50, 100};
-     for (int i=0; i < sizeof(N_array)/sizeof(N_array[0]); i++) {
-          int N = N_array[i];
-          int length = snprintf(NULL, 0, "%d", N );
-          char* str = malloc( sizeof(char) * (length + 5));
-          snprintf(str, length + 5, "%d.txt", N);
+	int N_array[3] = {20, 50, 100};
 
-          FILE *fp;
-          fp = fopen(str, "w+");
-          free(str);
+	for (int i=0; i < sizeof(N_array)/sizeof(N_array[0]); i++) {
+		int N = N_array[i];
+		int length = snprintf(NULL, 0, "%d", N );
+		char* str = malloc( sizeof(char) * (length + 5));
+		snprintf(str, length + 5, "%d.txt", N);
 
-          double delta = M_PI / (2 * N);
-          double sin_theta = computeDelta_sine(delta);
-          double cos_theta = computeDelta_cosine(delta);
-          double sine = 1;
-          double cosine = 0;
+		FILE *fp;
+		fp = fopen(str, "w+");
+		free(str);
 
-          fprintf(fp, "value         ours       lib        diff\n");
-          fprintf(fp, "---------------------------------------------------------\n");
+		int32_t pi = 0b00000011001001000011111101110000;
 
-          for (int j=0; j < N*4; j++) {
-               if (j == 0) {
-                    sine = 0;
-                    cosine = 1;
-               } else if (j == N) {
-                    sine = 1;
-                    cosine = 0;
-               } else if (j == 2*N) {
-                    sine = 0;
-                    cosine = -1;
-               } else if (j == 3*N) {
-                    sine = -1;
-                    cosine = 0;
-               } else {
-                    double old_sine = sine;
-                    sine = recurrence_sin(sin_theta, cos_theta, sine, cosine);
-                    cosine = recurrence_cos(sin_theta, cos_theta, old_sine, cosine);
-               }
-               double theta = j*delta;
-               double theta_deg = theta*180/M_PI;
-               fprintf(fp, "sin(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, sine, sin(theta), sine-sin(theta));
-               fprintf(fp, "cos(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, cosine, cos(theta), cosine-cos(theta));
-          }
-          fclose(fp);
-     }
-     return 0;
+		// Commented out for project 2
+		double delta = M_PI / (2 * N);
+		double sin_theta = computeDelta_sine(delta);
+		double cos_theta = computeDelta_cosine(delta);
+		double sine = 1;
+		double cosine = 0;
+
+		fprintf(fp, "value         ours       lib        diff\n");
+		fprintf(fp, "---------------------------------------------------------\n");
+
+		for (int j=0; j < N*4; j++) {
+			if (j == 0) {
+				sine = 0;
+				cosine = 1;
+			} else if (j == N) {
+				sine = 1;
+				cosine = 0;
+			} else if (j == 2*N) {
+				sine = 0;
+				cosine = -1;
+			} else if (j == 3*N) {
+				sine = -1;
+				cosine = 0;
+			} else {
+				double old_sine = sine;
+				sine = recurrence_sin(sin_theta, cos_theta, sine, cosine);
+				cosine = recurrence_cos(sin_theta, cos_theta, old_sine, cosine);
+			}
+			double theta = j*delta;
+			double theta_deg = theta*180/M_PI;
+			fprintf(fp, "sin(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, sine, sin(theta), sine-sin(theta));
+			fprintf(fp, "cos(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, cosine, cos(theta), cosine-cos(theta));
+		}
+		fclose(fp);
+	}
+
+	
+	return 0;
 }
 
