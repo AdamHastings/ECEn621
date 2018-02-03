90d8997d578b6868e34a66ae88741bd73e3f6ba4
AdamHastings <itsadamhastings@gmail.com>
2018-02-02 18:49:05 -0700

    fixed some bugs

--- Project2/Project2.c
+++ Project2/Project2.c
@@ -8,61 +8,90 @@
 // Pi in 8.24 fixed-point: 00000011 00100100 00111111 01110000
 
 
-int32_t int_to_fixedpt(int x) {
-     return x << (24 - 1);
+uint32_t int_to_fixedpt(uint32_t x) {
+	 return x << 24;
 }
 
 // 8.24 fixed-point representation
-double fixedpt_to_float(int32_t x) {
-     int whole_bits = (x >> 24);
-     double fractional_bits = ((double) (((unsigned) x << 8) >> 8)) / (2 << (24 - 1));
-     double representation = (double) whole_bits + fractional_bits;
-     return representation;
+double fixedpt_to_float(uint32_t x) {
+	 uint32_t whole_bits = (x >> 24);
+	 double fractional_bits = ((double) (((unsigned) x << 8) >> 8)) / (2 << (24 - 1));
+	 double representation = (double) whole_bits + fractional_bits;
+	 return representation;
 }
 
 // Get the index of the most significant bit 
-int get_MSB (uint32_t x) {
-     int i = 0;
-     while (x != 0) {
-          i++;
-          x = x >> 1;
-     }
-     return i;
+uint32_t get_MSB (uint32_t x) {
+	 uint32_t i = 0;
+	 while (x != 0) {
+		i++;
+		x = x >> 1;
+	 }
+	 return i;
 }
 
-int computeDivision(uint32_t numerator, uint32_t denominator) {
-     
+uint32_t computeDivision(uint32_t numerator, uint32_t denominator) {
+	 
+
+     // printf("numerator: %x\n", numerator);
+     // printf("denominator: %x\n",  denominator);
+
+     // printf("numerator: %f\n", fixedpt_to_float(numerator));
+     // printf("denominator: %f\n",  fixedpt_to_float(denominator));
+
 	// First normalize denominator to between 0.5 and 1
-	int msb = get_MSB(denominator);
-	int shift = (msb - 24);
+	uint32_t msb = get_MSB(denominator);
+	uint32_t shift = (msb - 24);
 
+     // uint32_t d_norm = denominator;
 	uint32_t d_norm = denominator >> shift;
-	printf("Denominator: %f, MSB: %d, shift: %d, Result: %f\n", fixedpt_to_float(denominator), msb, shift, fixedpt_to_float(d_norm));
 
-	int interations = 1;
-	for (int i=0; i<interations; i++) {
- 	  numerator = numerator * (2-d_norm);
-	  d_norm = d_norm*(2-d_norm);
+
+	printf("Denominator: %f, MSB: %d, shift: %d, Result: %f\n", fixedpt_to_float(denominator), msb, shift, fixedpt_to_float(d_norm));
+     // int d_norm = denominator;
+
+	uint32_t iterations = 5;
+	for (uint32_t i=0; i<iterations; i++) {
+		// First normalize denominator to between 0.5 and 1
+		// int msb = get_MSB(d_norm);
+		// int shift = (msb - 24);
+
+		// uint32_t d_norm = d_norm >> shift;
+		// printf("Denominator: %f, MSB: %d, shift: %d, Result: %f\n", fixedpt_to_float(denominator), msb, shift, fixedpt_to_float(d_norm));
+          printf("numerator: %f\n", fixedpt_to_float(numerator));
+          printf("denominator: %f\n",  fixedpt_to_float(d_norm));
+
+          uint32_t subtraction = int_to_fixedpt(2) - d_norm;
+          printf("subtraction: %f\n\n", fixedpt_to_float(subtraction));
+
+		numerator = numerator * subtraction;
+		d_norm = d_norm * subtraction;
+
+
+          //numerator = numerator >> shift;
 	}
-	int32_t Q = numerator >> shift;
+     printf("numerator: %f\n", fixedpt_to_float(numerator));
+     printf("denominator: %f\n",  fixedpt_to_float(denominator));
+
+	uint32_t Q = numerator >> shift; // shift;
 	return Q;
 }
 
 
 double computeDelta_sine(double delta) {
-     return delta - (delta*delta*delta)/6 +(delta*delta*delta*delta*delta) / 120;
+	 return delta - (delta*delta*delta)/6 +(delta*delta*delta*delta*delta) / 120;
 }
 
 double computeDelta_cosine(double delta) {
-     return 1 - (delta*delta)/2 + (delta*delta*delta*delta)/24 - (delta*delta*delta*delta*delta*delta)/720;
+	 return 1 - (delta*delta)/2 + (delta*delta*delta*delta)/24 - (delta*delta*delta*delta*delta*delta)/720;
 }
 
 double recurrence_sin(double sin_theta, double cos_theta, double sine, double cosine) {
-     return sine*cos_theta + cosine*sin_theta;
+	 return sine*cos_theta + cosine*sin_theta;
 }
 
 double recurrence_cos(double sin_theta, double cos_theta, double sine, double cosine) {
-     return cosine*cos_theta - sine*sin_theta;
+	 return cosine*cos_theta - sine*sin_theta;
 }
 
 
@@ -71,9 +100,13 @@ double recurrence_cos(double sin_theta, double cos_theta, double sine, double co
 // Let's pick an arbitrary 8.24 fixed-point numbering system.
 // Also, we can convert to float for printing!
 int main() {
-	int N_array[3] = {20, 50, 100};
-
 	
+     uint32_t quotient = computeDivision(int_to_fixedpt(2), int_to_fixedpt(4));
+     printf("Quotient: %f\n", fixedpt_to_float(quotient));
+
+     /*
+     int N_array[3] = {20, 50, 100};
+
 	// pi in 8.24 fixed-point notation
 	uint32_t pi = 0b00000011001001000011111101110000;
 
@@ -88,16 +121,16 @@ int main() {
 		free(str);
 
 		// Commented out for project 2
-        //double delta_new = fixedpt_to_float(computeDivision(pi, int_to_fixedpt(2*N)));
-        int32_t delta_fxp = computeDivision(pi, int_to_fixedpt(2*N));
+		//double delta_new = fixedpt_to_float(computeDivision(pi, int_to_fixedpt(2*N)));
+		int32_t delta_fxp = computeDivision(pi, int_to_fixedpt(2*N));
 		double delta = M_PI / (2 * N);
-        printf("delta: %f, delta_fxp: %f\n", delta, fixedpt_to_float(delta_fxp));
+		printf("delta: %f, delta_fxp: %f\n", delta, fixedpt_to_float(delta_fxp));
 		double sin_theta = computeDelta_sine(delta);
 		double cos_theta = computeDelta_cosine(delta);
 		double sine = 1;
 		double cosine = 0;
 
-		fprintf(fp, "value         ours       lib        diff\n");
+		fprintf(fp, "value		 ours	 lib		diff\n");
 		fprintf(fp, "---------------------------------------------------------\n");
 
 		for (int j=0; j < N*4; j++) {
@@ -124,7 +157,7 @@ int main() {
 			fprintf(fp, "cos(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, cosine, cos(theta), cosine-cos(theta));
 		}
 		fclose(fp);
-	}
+	}*/
 
 	
 	return 0;
