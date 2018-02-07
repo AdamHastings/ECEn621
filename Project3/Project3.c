49ac916bf559a99831c9a3f3f6b6426457199dce
Adam Hastings <adam@CB461-EE11028.ee.byu.edu>
2018-02-07 15:47:53 -0700

    cleaned up comments. 45 degree bug has been fixed

--- Project3/Project3.c
+++ Project3/Project3.c
@@ -70,7 +70,7 @@ void rotationMode() {
 }
 
 void vectorMode() {
-
+	// TODO
 }
 
 
@@ -79,62 +79,6 @@ int main() {
 	rotationMode();
 	vectorMode();
 
-     /*
-     int N_array[3] = {20, 50, 100};
-
-	// pi in 8.24 fixed-point notation
-	uint32_t pi = 0b00000011001001000011111101110000;
-
-	for (int i=0; i < 1; i++) {
-		int N = N_array[i];
-		int length = snprintf(NULL, 0, "%d", N );
-		char* str = malloc( sizeof(char) * (length + 5));
-		snprintf(str, length + 5, "%d.txt", N);
-
-		FILE *fp;
-		fp = fopen(str, "w+");
-		free(str);
-
-		// Commented out for project 2
-		//double delta_new = fixedpt_to_float(computeDivision(pi, int_to_fixedpt(2*N)));
-		int32_t delta_fxp = computeDivision(pi, int_to_fixedpt(2*N));
-		double delta = M_PI / (2 * N);
-		printf("delta: %f, delta_fxp: %f\n", delta, fixedpt_to_float(delta_fxp));
-		double sin_theta = computeDelta_sine(delta);
-		double cos_theta = computeDelta_cosine(delta);
-		double sine = 1;
-		double cosine = 0;
-
-		fprintf(fp, "value		 ours	 lib		diff\n");
-		fprintf(fp, "---------------------------------------------------------\n");
-
-		for (int j=0; j < N*4; j++) {
-			if (j == 0) {
-				sine = 0;
-				cosine = 1;
-			} else if (j == N) {
-				sine = 1;
-				cosine = 0;
-			} else if (j == 2*N) {
-				sine = 0;
-				cosine = -1;
-			} else if (j == 3*N) {
-				sine = -1;
-				cosine = 0;
-			} else {
-				double old_sine = sine;
-				sine = recurrence_sin(sin_theta, cos_theta, sine, cosine);
-				cosine = recurrence_cos(sin_theta, cos_theta, old_sine, cosine);
-			}
-			double theta = j*delta;
-			double theta_deg = theta*180/M_PI;
-			fprintf(fp, "sin(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, sine, sin(theta), sine-sin(theta));
-			fprintf(fp, "cos(%6.2f) %10.6f %10.6f %10.6f\n",theta_deg, cosine, cos(theta), cosine-cos(theta));
-		}
-		fclose(fp);
-	}*/
-
-	
 	return 0;
 }
 
