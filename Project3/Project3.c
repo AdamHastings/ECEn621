18d7e77c78bc3547b2e069b5b9fd41d6d7b54349
Matthew Salvatore Viglione <matthewviglione@gmail.com>
2018-03-02 23:50:37 -0700

    Working properly

--- Project3/Project3.c
+++ Project3/Project3.c
@@ -62,12 +62,21 @@ void rotationMode() {
 			}
 		}
 
-		// Write to file
-		fprintf(sinFP, "sin(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, y, sin(i * (M_PI/180.0)), y-sin(i * (M_PI/180.0)));
-		fprintf(cosFP, "cos(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, x, cos(i * (M_PI/180.0)), x-cos(i * (M_PI/180.0)));
-		if (i != 90 && i != -90) // Tan(+/-90) is undefined
-			fprintf(tanFP, "tan(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, y/x, tan(i * (M_PI/180.0)), y/x-tan(i * (M_PI/180.0)));
+          // Check results and write to files
 
+          // sin
+          double sin_mathlib = sin(i * (M_PI/180.0));
+          fprintf(sinFP, "sin(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, y, sin_mathlib, y-sin_mathlib);
+
+          // cos
+          double cos_mathlib = cos(i * (M_PI/180.0));
+          fprintf(cosFP, "cos(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, x, cos_mathlib, x-cos_mathlib);
+
+          // tan
+          if (i != 90 && i != -90) { // Tan(+/-90) is undefined
+               double tan_mathlib = tan(i * (M_PI/180.0));
+               fprintf(tanFP, "tan(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, y/x, tan_mathlib, y/x-tan_mathlib);
+          }
  	}
 
  	// Close the files
@@ -81,18 +90,27 @@ void vectorMode() {
 
 	// Create file pointer and open file
 	FILE *arctanFP;
-	arctanFP = fopen("arctan.txt", "w+");
+     arctanFP = fopen("arctan.txt", "w+");
+
+	FILE *sqrtFP;
+	sqrtFP = fopen("dist.txt", "w+");
 
 	// Write header
 	fprintf(arctanFP, "Value	 	 CORDIC	 	math.h		Difference\n");
 	fprintf(arctanFP, "----------------------------------------------------------\n");
 
+     // Write header
+	fprintf(sqrtFP, "Value	 	 CORDIC	 	math.h		Difference\n");
+	fprintf(sqrtFP, "----------------------------------------------------------\n");
+
 	// Test angles in the range [-98, 98]
 	for (double i=-98; i <= 98; i++) {
 
-		double z = 0;
-		double x = 1;
-		double y = i * ((2 * M_PI)/360); // Convert to radians
+          double x0 = 0;
+          double y0 = 1;
+          double x = x0;
+		double y = y0;
+		double z = i * ((2 * M_PI)/360);
 
 		// Hard code values that don't need to be approximated
 		if (i == -90) {
@@ -120,23 +138,30 @@ void vectorMode() {
 			}
 		}
 
-		// Write to file
-		double arctan_mathlib = atan(i * (M_PI/180.0));
-          fprintf(arctanFP, "arctan(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, z, arctan_mathlib, z-arctan_mathlib);
+          // Write to file
+          if (i != 90 && i != -90 && i !=0) {
+               double arctan_mathlib = i * (M_PI/180.0) + atan(y0/x0);
+               fprintf(arctanFP, "arctan(%6.2f)    %10.6f     %10.6f      %11.8f\n",i, z, arctan_mathlib, z-arctan_mathlib);
+          }
 
-          // do something about 90 and -90
+          if(i>0 && i != 90) {
+               double dist_mathlib = y0;
+               double k_inv = 0.607252935; // 1/K
+
+               // double sqrt_cordic = k_inv*pow(2,1+x);
+               fprintf(sqrtFP, "dist(0,%6.2f)    %10.6f     %10.6f      %11.8f\n",y0, x*k_inv, dist_mathlib, x*k_inv-dist_mathlib);
+          }
+
  	}
 
  	// Close the file
 	fclose(arctanFP);
+	fclose(sqrtFP);
 }
 
-
 int main() {
-
 	rotationMode();
 	vectorMode();
-
 	return 0;
 }
 
