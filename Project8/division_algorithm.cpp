1a35e8af7dfd56a85e8fb0d760b6d325875cc3e3
Matthew Salvatore Viglione <matthewviglione@gmail.com>
2018-03-23 16:23:22 -0600

    Map approximation working

--- Project8/division_algorithm.cpp
+++ Project8/division_algorithm.cpp
@@ -12,24 +12,16 @@ using namespace std;
 
 map<double, double> reciprocal_table;
 
-
-
-double getClosest(double pos);
-double divide(double x, double d);
-void populate_table();
-int get_next_digit(double w);
-
-
 void populate_table(){
-	cout << "Reciprocal Table:" << endl;
-	// cout << fixed << setprecision(32);
+	// cout << "Reciprocal Table:" << endl;
+	cout << fixed << setprecision(32);
 	for(double i=0.5; i < 1; i = i + pow(2,-K)) {
 		double unrounded = 1 / i;
 		double rounded_up = ceil ( unrounded * pow(2,K)) / pow(2,K);
 		reciprocal_table[i] = rounded_up;
-		cout << i << " : " << reciprocal_table[i] << endl;
+		// cout << i << " : " << reciprocal_table[i] << endl;
 	}
-	cout << endl;
+	// cout << endl;
 }
 
 int get_next_digit(double w) {
@@ -39,66 +31,28 @@ int get_next_digit(double w) {
 double divide(double x, double d) {
 
 	double x_prime = x * reciprocal_table[d];
-
-
-
-	// double d_prime = d * reciprocal_table[d];
-
-
+	double d_prime = d * reciprocal_table[d];
 	// cout << "m :" << reciprocal_table[d] << endl;
 	// cout << "x_prime: " << x_prime << endl;
 	// cout << "d_prime: " << d_prime << endl;
 	// cout << "ideal: " << x_prime / d_prime << endl << endl;
 
-     cout << "d: " << d << endl;
-     cout << "rt[d]: " << getClosest(d) << endl;
-
 	double q = 0;
-	// double w = x_prime;
-	// // cout << "w0: " << w << endl;
-	// int D;
+	double w = x_prime;
+	// cout << "w0: " << w << endl;
+	int D;
 
 
-	// for (int i=1; i<=K; i++) {
-	// 	// cout << "D" << i-1 << ": " << D << endl;
-	// 	D = get_next_digit(w);
-	// 	q = q + D * pow(RADIX,-(i));
-	// 	w = RADIX * w - d_prime * D;
-	// 	// cout << "w" << i << ": " << w << endl;
-	// }
+	for (int i=1; i<=K; i++) {
+		// cout << "D" << i-1 << ": " << D << endl;
+		D = get_next_digit(w);
+		q = q + D * pow(RADIX,-(i));
+		w = RADIX * w - d_prime * D;
+		// cout << "w" << i << ": " << w << endl;
+	}
 	return q;
 }
 
-double getClosest(double pos){
-
-     // if(pos <= reciprocal_table.begin()->first) return reciprocal_table.begin()->first;   // if pos is less than all map elements, or equal to the first one, return the first one
-     // if(reciprocal_table.size() < 2) return reciprocal_table.begin()->first;              // if only one element in map, return it
-
-
-     int i=0;
-
-     for (auto it = reciprocal_table.begin(); it != reciprocal_table.end(); it++) {
-          double next_val = next(it)->first;
-          double curr_val = it->first;
-
-          cout << "iteration: " << i << endl << "\tcurr_val: " << curr_val << "\tnext_val: " << next_val << endl;
-
-
-          // double test = .62;
-
-          // if(test < next_val) { // if pos is less than next map element
-          //      cout << "x: " << curr_val << endl;
-          //      cout << "xx: " <<  next_val << endl;
-          //      return curr_val;
-          // }
-          i++;
-     }
-
-
-
-     cout << "ERROR" << endl;
-     return -1;
-}
 
 int main() {
 
@@ -108,8 +62,8 @@ int main() {
 	}
 
 	double x = 0.66666666;
-	double dd = 0.61;
+	double d = 0.51171875;
 	populate_table();
-	cout << "Expected result: " << x / dd << endl;
-	cout << "Computed result: " << divide(x, dd) << endl;
+	cout << "Expected result: " << x / d << endl;
+	cout << "Computed result: " << divide(x, d) << endl;
 }
--- Project8/test2.cpp
+++ Project8/test2.cpp
@@ -3,19 +3,58 @@
 #include <math.h>
 #include <stdio.h>
 #include <iomanip>
+#include <time.h>       /* time */
+#include <stdlib.h>     /* srand, rand */
 using namespace std;
 
-int main() {
+map<double, double> reciprocal_table;
 
-     map<int, int> myMap;
-
+double mapGet(double i){
      try{
-          myMap.at(2);
+          reciprocal_table.at(i);
      } catch(const std::out_of_range& ex) {
-          cout << "doesn't exist from: \"" << ex.what() << "\" "<< endl;
+          cout << i << " doesn't exist in: \"" << ex.what() << "\" "<< endl;
      }
-
-
-	for (auto it = myMap.begin(); it != myMap.end(); it++)
-          cout << it->first << " -> " << it->second << endl;
 }
+
+double getClosest(double pos){
+     if(reciprocal_table.size() < 1) return 0;                                            // if map is empty
+     if(reciprocal_table.size() < 2) return reciprocal_table.begin()->first;              // if only one element in map, return it
+     if(pos <= reciprocal_table.begin()->first) return reciprocal_table.begin()->first;   // if pos is less than all map elements, or equal to the first one, return the first one
+
+     for (auto it = reciprocal_table.begin(); it != reciprocal_table.end(); it++) {
+          auto it_next = next(it);
+
+          // cout << "\tcurr_val: " << curr_val;
+
+          if(it_next == reciprocal_table.end()){  // if it is the last element in the map
+               return it->first;
+          } else {
+               if(pos < it_next->first) {                    // if pos is less than next map element
+                    // it is lower bound, next is upper bound
+                    if(pos - it->first <= it_next->first - pos)      // if pos is closer to it
+                         return it->first;
+                    else
+                         return it_next->first;
+               }
+          }
+     }
+     return 0; // shouldn't ever get here
+}
+
+double f_Rand(double fMin, double fMax){
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+int main() {
+     srand (time(NULL));
+     for(double i=20; i<=50; i+=.5)
+           reciprocal_table[i]=i;
+     for(int i=0; i<20; i++){
+          double my_rand = f_Rand(0,70);
+          cout << "getClosest(" << my_rand << "): " << getClosest(my_rand) << endl;
+     }
+}
+
+
