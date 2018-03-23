a56e4561b58c6c36d2a9f2ce5a385d17d632613b
Matthew Viglione <matthewviglione@gmail.com>
2018-03-23 17:26:02 -0600

    Project complete. Division and random testing working properly.

--- Project8/mapApproximation.cpp
+++ Project8/mapApproximation.cpp
@@ -17,40 +17,42 @@ double mapGet(double i){
      }
 }
 
+// return a random float in the specified range
+double randf(double fMin, double fMax){
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+// return the key to the nearest map element that exists
 double getClosest(double pos){
      if(reciprocal_table.size() < 1) return 0;                                            // if map is empty
      if(reciprocal_table.size() < 2) return reciprocal_table.begin()->first;              // if only one element in map, return it
      if(pos <= reciprocal_table.begin()->first) return reciprocal_table.begin()->first;   // if pos is less than all map elements, or equal to the first one, return the first one
 
      for (auto it = reciprocal_table.begin(); it != reciprocal_table.end(); it++) {
-          auto it_next = next(it);
-
-          if(it_next == reciprocal_table.end()){  // if it is the last element in the map
+          auto it_next = next(it);                     // get iterator to next map element
+          if(it_next == reciprocal_table.end()){       // if it is the last element in the map
                return it->first;
           } else {
-               if(pos < it_next->first) {                    // if pos is less than next map element
-                    // it is lower bound, next is upper bound
-                    if(pos - it->first <= it_next->first - pos)      // if pos is closer to it
+               if(pos < it_next->first) {                             // if pos is less than next map element
+                    // it is lower bound, it_next is upper bound
+                    if(pos - it->first <= it_next->first - pos)      // if pos is closer to current element (it)
                          return it->first;
-                    else
+                    else                                             // pos is closer to next element (it_next)
                          return it_next->first;
                }
           }
      }
-     return 0; // shouldn't ever get here
+     return 0; // shouldn't ever get here, error condition
 }
 
-double f_Rand(double fMin, double fMax){
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
-}
 
 int main() {
      srand (time(NULL));
      for(double i=20; i<=50; i+=.5)
            reciprocal_table[i]=i;
      for(int i=0; i<20; i++){
-          double my_rand = f_Rand(0,70);
+          double my_rand = randf(0,70);
           cout << "getClosest(" << my_rand << "): " << getClosest(my_rand) << endl;
      }
 }
