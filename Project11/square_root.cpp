8721e927c3d7aa5b4686bbbd613bddba67199ce8
Adam Hastings <adam@CB461-EE11028.ee.byu.edu>
2018-04-13 13:22:14 -0600

    working now, bug free

--- Project11/square_root.cpp
+++ Project11/square_root.cpp
@@ -11,17 +11,9 @@ using namespace std;
 #define A 1
 #define RHO ((A)/(RADIX * 1.0 - 1))
 #define K_MAX (64 + DELTA)
-// #define NUM_TESTS 1000
-#define NUM_TESTS 1
+#define NUM_TESTS 1000
 #define DELTA 1
 
-// double getLowerBound(int d, double S, int k) {
-// 	return 2 * S * (d - RHO) + pow((d - RHO), 2) * pow(RADIX, -(k));
-// }
-
-// double getUpperBound(int d, double S, int k) {
-// 	return 2 * S * (d + RHO) + pow((d + RHO), 2) * pow(RADIX, -(k));
-// }
 
 // Interpret digitArray as a number with radix = 2
 double interpretArray(int x[K_MAX]) {
@@ -34,9 +26,8 @@ double interpretArray(int x[K_MAX]) {
 int getNextDigit(double S, int k, double x, double z) {
 	double B_1_0 = (2*S) + pow(2, -k) - x;
 	double B_0_n1 = (-2*S) + pow(2, -k) - x;
-	cout << "B10: " << B_1_0 << ", B0n1: " << B_0_n1 << endl;
 	double scaled_residual = 2 * z;
-	cout << "scaled_residual: " << scaled_residual << endl;
+
 	if (scaled_residual >= B_1_0)
 		return 1;
 	else if (scaled_residual >= B_0_n1)
@@ -47,23 +38,16 @@ int getNextDigit(double S, int k, double x, double z) {
 
 double computesqrt(int (&x)[K_MAX]) {
 
-	cout << "x = ";
-	for (int i=0; i < K_MAX; i++) {
-		cout << x[i];
-	}
-	cout << endl;
-
-
-
 	double X, S, old_S, s, z;
+	
+	// k = 0
+	X = x[0]; 
 
 	// k = 1
-	X = x[1] * pow(2, -1);
+	X += x[1] * pow(2, -1);
 	s = 0;
 	S = 0;
 	z = pow(2, 1) * (X - pow(S, 2));
-	cout << "X_1 = " << X << endl;
-	cout << "z_1 = " << z << endl << endl;
 	old_S = S;
 
 	// k = 2
@@ -71,18 +55,15 @@ double computesqrt(int (&x)[K_MAX]) {
 	s = 1;
 	S = 0.5;
 	z = pow(2, 2) * (X - pow(S, 2));
-	cout << "X_2 = " << X << endl;
-	cout << "z_2 = " << z << endl << endl;
+
 
 	// k >= 3
 	for (int i=3; i < K_MAX-1; i++) {
 		X += x[i] * pow(2, -i);
-		// 90% sure the problem is in getNextDigit
 		s = getNextDigit(old_S, (i-1), x[i], z);
 		old_S = S;
 		S += s * pow(2, -(i-1));
 		z = (2*z) + x[i] - (2*s*(old_S + S));
-		cout << "z: " << z << ", S: " << S << ", s: " << s << endl << endl;
 	}
 
 	return S;
@@ -100,8 +81,7 @@ void createRandInput(int x[K_MAX])
 
 int main() {
 
-	// srand (time(NULL)); // seed rand()
-	srand(100);
+	srand (time(NULL)); // seed rand()
 
 	// set cout precision to 16 decimal places
 	cout << fixed << setprecision(16);
